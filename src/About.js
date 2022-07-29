476ad6f00da8daab05e0334f276648987eaa472d
ebharathi123 <insideout123eb@gmail.com>
2022-07-29 11:57:38 +0530

    first

--- src/About.js
+++ src/About.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className="container my-5">
+    <div className="container my-5" style={{}}>
           <h6 className='text-center'>This Page was created by using ReactJS,MongoDB,Express,JavaScript,Bootstrap,HTML and CSS.</h6>
     </div>
   )
